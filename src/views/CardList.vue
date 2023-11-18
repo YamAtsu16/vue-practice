1f2adf3f3bcec93d4e9fb83ed1072842794d627c
YamAtsu16 <00kisaragi16@gmail.com>
2023-11-18 14:37:33 +0900

    Cardファイル追加
    slotの実装

---
+++ src/views/CardList.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>Card</h1>
+  <div class="container">
+    <Card title="Title" class="card">
+      <div class="contents">
+        <span>no message</span>
+      </div>
+      <template #footer>
+        <div>footer</div>
+      </template>
+    </Card>
+    <Card title="Icons" class="card">
+      <div class="contents">
+        <RocketIcon />
+        <RocketIcon />
+        <RocketIcon />
+      </div>
+    </Card>
+    <Card title="Icons with Text" class="card">
+      <div class="contents">
+        <RocketIcon />
+        <span class="with-text-card">Rocket</span>
+        <RocketIcon />
+      </div>
+      <template #footer>
+        Hello World!!
+      </template>
+    </Card>
+  </div>
+</template>
+<script setup lang="ts">
+  import Card from '../components/Card.vue';
+  import RocketIcon from '../components/RocketIcon.vue';
+</script>
+<style>
+span {
+  font-size: 18px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.card {
+  margin-bottom: 24px;
+}
+.card .contents {
+  margin-top: 8px;
+  display: flex;
+}
+</style>
